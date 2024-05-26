c73e40215d9629ace696811e251622d091cc1c55
Rafal Pylko <rafal.pylko03@gmail.com>
2024-05-27 00:22:16 +0200

    test videos

--- Datasets/F1cars/detection.py
+++ Datasets/F1cars/detection.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("results/train_colab_1/weights/best_colab_1.pt")
 
-photo = model.predict(source="data/test/images/", show=True, conf=0.6, save=True)
+photo = model.predict(source="data/videos/Imola15s.mp4", show=True, conf=0.5, save=True, line_thickness=1, hide_labels=True, hide_conf=True)
