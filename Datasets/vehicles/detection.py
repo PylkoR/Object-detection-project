6b4bb3d27b77d5f862ca38ec6b8e8bd6b2c7ab2c
Rafal Pylko <rafal.pylko03@gmail.com>
2024-06-08 15:05:37 +0200

    adding not tracked files

--- Datasets/vehicles/detection.py
+++ Datasets/vehicles/detection.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("best_s_57.pt")
+model = YOLO("best.pt")
 
-photo = model.predict(source="test_predict/", show=True, conf=0.5, save=True)
+photo = model.predict(source="test_predict/", show=True, conf=0.5, save=True, line_thickness=2)
--- Datasets/vehicles/trainer.py
+++ Datasets/vehicles/trainer.py
@@ -3,6 +3,6 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 if __name__ == '__main__':
-    model.train(data="data_vehicles.yaml", imgsz=640, epochs=10, batch=-1, amp=False)
+    model.train(data="data_vehicles.yaml", imgsz=640, epochs=1, batch=8, amp=False)
 
 #yolo task=detect mode=train epochs=75 data=data_vehicles.yaml model=yolov8n.pt imgsz=640
